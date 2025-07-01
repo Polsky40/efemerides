f642cc25aaffc979d0de7a327858585ccab9fd85
Pablo <99818876+Polsky40@users.noreply.github.com>
2025-07-01 16:19:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
+app = Flask(__name__)
+CORS(app)  # ← habilita CORS para todos los orígenes
 import swisseph as swe
 from datetime import datetime
 
